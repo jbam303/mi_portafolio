8db96a46e568a436c00dd2531f4a75e62f17af39
rei_chikita <jbam3019@gmail.com>
2025-08-29 00:02:41 -0400

    Arreglo pagina de proyectos y logicas de vista

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    hora = datetime.now().hour
+    if hora < 12:
+        saludo = "Buenos días!!"
+    elif hora < 19:
+        saludo = "Buenas tardes!!"
+    else:
+        saludo = "Buenas noches!!"
+    return render(request, 'index.html', {'saludo': saludo})
 
 def proyectos(request):
-    #projects = [
-    #    { "title": "Proyecto 1", "description": "Descripción del proyecto 1", "in_progress": True, "image1": "" },
-    #    { "title": "Proyecto 2", "description": "Descripción del proyecto 2", "in_progress": False, "image2": "" },
-    #    { "title": "Proyecto 3", "description": "Descripción del proyecto 3 ", "in_progress": True, "image3": "" },
-    #]
-    return render(request, 'proyectos.html')
+    projects = [
+        { "title": "Proyecto 1", "description": "Descripción del proyecto 1", "in_progress": True, "image": "images/johan.jpg" },
+        { "title": "Proyecto 2", "description": "Descripción del proyecto 2", "in_progress": False, "image": "images/johan.jpg" },
+        { "title": "Proyecto 3", "description": "Descripción del proyecto 3 ", "in_progress": True, "image": "images/johan.jpg" }
+    ]
+    return render(request, 'proyectos.html', {'projects': projects})
 
 def contacto(request):
     return render(request, 'contacto.html')
